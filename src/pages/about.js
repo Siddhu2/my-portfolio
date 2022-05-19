c7af4673b87f2d9c7866ad15b930dc3ca8a3a227
siddharth <mksiddhu2@gmail.com>
2022-05-19 10:57:07 +0530

    Completed home page and started with about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,20 @@
 function About(props)
 {
     return (
-        <div>
-            <h1>About</h1>
+        <div style={{ 
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/img/home.jpg'})`,
+            backgroundSize: 'cover',
+            height: '100vh',
+            width: '100vw',
+            backgroundAttachment: 'fixed',
+            backgroundRepeat: 'no-repeat',
+          }}> 
+            <div>I have completed my Bachelor of Engineering in Computer Science. Currently 
+                working as software tester in a MNC. I started my profession at 2017.
+                I have a keen interest in programming and used to do mini projects on my own. 
+                My hobbies include reading books, listening to music and watching movies. I do
+                write blogs on my own which you can find on my <a href="https://medium.com/@mksiddhu2" target="_blank">medium account.</a> 
+                 </div>
             </div>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,30 @@
-function Home()
+import classes from './home.module.css';
+
+function Home(props)
 {
     return (
-        <div className="home">
-            <h3>This is home page</h3>
+        <div style={{ 
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/img/home.jpg'})`,
+            backgroundSize: 'cover',
+            height: '100vh',
+            width: '100vw',
+            backgroundAttachment: 'fixed',
+            backgroundRepeat: 'no-repeat',
+          }}> 
+        <div className={classes.home}>
+            Siddharth Murugan
+        </div>
+        <div className={classes.profession}>
+            Software Tester
+            </div>
+        <div className={classes.skillset}>
+            Having hands on experience in testing with:
+            <br>
+        </br>
+        <div className={classes.tools}>
+        Cypress | WebdriverIO | Appium | Jenkins | Git
+        </div>
+        </div>
         </div>
     )
 }
